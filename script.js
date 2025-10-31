37b14c49df25c9037c6b9844fa26378b8e6db337
HorrySheet11 <marinnathan11@gmail.com>
2025-10-31 21:01:03 +0800

    adding factory functions for game logic

---
+++ script.js
@@ -0,0 +1,72 @@
+function gameControl () {
+    let gameBoard  = [["", "", ""], ["", "", ""], ["", "", ""]];
+    let currentPlayer = "X";
+    let gameActive = true;
+
+    let players = {
+        player1 : "X",
+        player2 : "O"
+    };
+
+    const winningConditions = [
+        // Rows
+        [[0, 0], [0, 1], [0, 2]],
+        [[1, 0], [1, 1], [1, 2]],
+        [[2, 0], [2, 1], [2, 2]],
+        // Columns
+        [[0, 0], [1, 0], [2, 0]],
+        [[0, 1], [1, 1], [2, 1]],
+        [[0, 2], [1, 2], [2, 2]],
+        // Diagonals
+        [[0, 0], [1, 1], [2, 2]],
+        [[0, 2], [1, 1], [2, 0]]
+    ];
+
+    const checkWin = ()=> {
+        for (let condition of winningConditions) {
+            const [a, b, c] = condition;
+            if (gameBoard[a[0]][a[1]] !== "" &&
+                gameBoard[a[0]][a[1]] === gameBoard[b[0]][b[1]] &&
+                gameBoard[a[0]][a[1]] === gameBoard[c[0]][c[1]]) {
+                    const winner = gameBoard[a[0]][a[1]];
+                return true;
+            }
+        }
+        return false;
+    }
+
+    const checkDraw = ()=>  {
+        for (let row of gameBoard) {
+            for (let cell of row) {
+                if (cell === "") {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    const makeMove = (row, col) => {
+        if (gameActive && gameBoard[row][col] === "") {
+            gameBoard[row][col] = currentPlayer;
+            if (checkWin()) {
+                gameActive = false;
+                console.log(`Player ${currentPlayer} wins!`);
+            } else if (checkDraw()) {
+                gameActive = false;
+                console.log("It's a draw!");
+            } else {
+                currentPlayer = currentPlayer === players.player1 ? players.player2 : players.player1;
+            }
+        }
+    }
+
+    const resetGame = () => {
+        gameBoard = [["", "", ""], ["", "", ""], ["", "", ""]];
+        currentPlayer = players.player1;
+        gameActive = true;
+    }
+    return {
+        makeMove, resetGame
+    }
+}
